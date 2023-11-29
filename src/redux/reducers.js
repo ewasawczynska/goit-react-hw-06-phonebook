b98c6a99f930dfef783537f2935abf0eb7ff5ae0
ewasawczynska <sawczynska.e@gmail.com>
2023-11-29 21:06:14 +0100

    Add Redux reducers

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,1 +1,26 @@
 import { createReducer } from "@reduxjs/toolkit";
+import { addContact, deleteContact, setFilter } from "./actions";
+
+// CONTACTS
+
+const contactsInitialState = [];
+
+export const contactsReducer = createReducer(contactsInitialState, {
+    [addContact]: (state, action) => {
+      state.push(action.payload);
+    },
+    [deleteContact]: (state, action) => {
+      const index = state.findIndex(contact => contact.id === action.payload);
+      state.splice(index, 1);
+    }
+  });
+
+  // FILTER
+
+  const filtersInitialState = [];
+
+  export const filtersReducer = createReducer(filtersInitialState, {
+    [setFilter]: (state, action) => {
+      state.status = action.payload;
+    },
+  });
